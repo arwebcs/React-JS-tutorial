71f8c1da9709aec23842fd33daf65393929a7868
Soumyanjan Dey <arwebcs@gmail.com>
2024-02-08 13:35:10 +0530

    React API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("url", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer  {"id":18,"name":"Developer ","email":"Dev@dev.com","password":"Dev123","phone":"000000000","address":"Kolkata ","is_staff":"n","bank_details":"Bank details ","due_amount":1,"last_month_dues":0,"last_month_paid":"n","created_at":"2024-01-18T13:24:01.000Z"}`,
+      },
+      //   body: {},
+    }).then((res) => {
+      res.json().then((res) => {
+        setData(res);
+      });
+    });
+  }, []);
+
+  console.log(data);
+
+  return <div className="App">hh</div>;
 }
 
 export default App;
